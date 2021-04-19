291a1c4d7339d4b551ce63109284cdacc458be15
manisingh2000 <mani.singh_cs18@gla.ac.in>
2021-04-19 12:00:28 +0530

    commit

---
+++ landingpage.php
@@ -0,0 +1,140 @@
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Travel and Tourism | Landing Page</title>
+      <link rel="stylesheet" href="stylee.css">
+      <style>
+            html,
+            body {
+                  margin: 0;
+                  padding: 0;
+                  width: 100%;
+            }
+
+            body {
+                  width: 100%;
+                  height: 100vh;
+                  background-image: url("images/mike-swigunski-_5xgPKGepgc-unsplash.jpg");
+                  background-position: center;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+                  display: table;
+                  font-family: Montserrat;
+            }
+
+            .nav {
+                  position: fixed;
+                  top: 0;
+                  left: 0;
+                  margin: 0;
+                  padding: 0;
+                  width: 100%;
+                  height: 80px;
+            }
+
+            .logo {
+                  
+                  color: #FF4500;
+                  font-size: 20px;
+                  line-height: 40px;
+                  padding: 1px 20px;
+                  border: 1px solid #00FFFF;
+                  border-radius: 4px;
+                  margin: 1.5em;
+                  display: inline-block;
+                  list-style: none;
+                  font-weight: Lighter;
+            }
+
+            .menu {
+                  float: right;
+                  line-height: 60px;
+                  margin: 0.1em;
+            }
+
+            .menu ul {
+                  list-style: none;
+            }
+
+            .menu ul li {
+                  display: inline-block;
+            }
+
+            .menu ul li a {
+                  text-decoration: none;
+                  color: #fff;
+                  font-size: 16px;
+                  font-weight: Lighter;
+                  padding: 0 20px;
+            }
+
+            ul li:nth-child(2) a {
+                  color: #00FF7F;
+                  border: 1px solid #00FFFF;
+                  padding: 10px 20px;
+                  border-radius: 4px;
+            }
+
+            ul li:nth-child(1) a {
+                  color: #00FF7F;
+                  border: 1px solid #00FFFF;
+                  padding: 10px 20px;
+                  border-radius: 4px;
+            }
+
+            .header {
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  transform: translate(-50%, -50%);
+                  text-align: center;
+            }
+
+            .header h1 {
+                  color: #FFD700;
+                  font-size: 64px;
+                  font-weight: 400px;
+                  margin-bottom: 8px;
+                  font-style: italic;
+                  text-shadow: 4px 4px 4px RoyalBlue;
+            }
+
+            .header p {
+                  color: #7CFC00;
+                  font-size: 28px;
+                  font-weight: bold;
+                  text-shadow: 4px 4px 4px OrangeRed;
+                  font-style: italic;
+                  font-weight: 1000;
+            }
+      </style>
+</head>
+
+<body>
+      <div class="wrapper">
+            <div class="nav">
+                  <div class="logo">
+                        Travel and Tourism
+                  </div>
+                  <div class="menu">
+                        <ul>
+
+                              <li><a href="login.php" target="_blank">Login</a></li>
+                              <li><a href="demo.php" target="_blank">SignUp</a></li>
+
+                        </ul>
+                  </div>
+            </div>
+            <div class="header">
+                  <h1>Travel and Tourism</h1>
+                  <p>Let's Explore The World
+                  </p>
+
+            </div>
+      </div>
+</body>
+
+</html>
