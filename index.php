13f8950656b9b601f47eaf6773a73b20ade53757
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2021-04-20 00:50:30 +0530

    commit

--- index.php
+++ index.php
@@ -18,46 +18,55 @@
 
 <body>
   
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top  scrolling-navbar">
+  <div class="container">
   
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top  ">
-        <div class="container">
-          <a class="navbar-brand" href="#">Travel and Tourism</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-      
-          <div class="collapse navbar-collapse" id="Sticky-nav">
-            <ul class="navbar-nav ml-auto">
-      <li class="nav-item ">
-                <a class="nav-link"  href="#">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"  >Pages</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="About.php">About Us</a>
-                  <a class="dropdown-item" href="Contact_us.php">Contact Us</a>
-                  </div>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle"   data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" >Destination</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="package.html#historic">Historical Places</a>
-                  <a class="dropdown-item" href="#destination">Destination List</a>
-                  </div>
-              
-              </li>
-              <li class="nav-item">
-                <a class="nav-link"    href="package.html">Packages</a>
-              </li>
-             <li class="nav-item">
-                <a class="nav-link"    href="#services">Services</a>
-              </li>
-      
+    <a class="navbar-brand" href="#">Travel and Tourism</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="Sticky-nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+            aria-expanded="false">Pages</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="About.php">About Us</a>
+            <a class="dropdown-item" href="Contact_us.php">Contact Us</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+            aria-expanded="false">Destination</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="package.html#historic">Historical Places</a>
+            <a class="dropdown-item" href="#destination">Destination List</a>
+          </div>
+
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="package.html">Packages</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="booking.html">Booking</a>
+        </li>
+
       </ul>
-      </div>
-      </div>
-      </nav>
-      
+    </div>
+  </div>
+</nav>
+</header>
+
 
       
     
